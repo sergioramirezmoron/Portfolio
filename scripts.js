eda8a633e61eb40a67e951112d39eac5f8773235
sergioramirezmoron <sergiothd50@gmail.com>
2025-08-08 14:52:11 +0200

    actualizar hasta proyectos

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Botón modo oscuro
-  const btn = document.getElementById("toggle-dark");
-  if (btn) {
-    btn.addEventListener("click", () => {
-      document.body.classList.toggle("dark");
-    });
-  }
-});
